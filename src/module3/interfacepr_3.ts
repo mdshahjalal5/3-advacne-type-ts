9a58c36f806cf7d132a6d86edb34b4833a003442
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-24 14:49:36 +0600

    done

--- src/module3/interfacepr_3.ts
+++ src/module3/interfacepr_3.ts
@@ -2,3 +2,26 @@
 //!n! furab => function => array => object
 
 // ? In TypeScript, the extends keyword is used in interface declarations to create a new interface that extends an existing interface. When one interface extends another, it inherits all the properties and methods of the base interface, and can define its own additional properties and methods.
+
+interface school {
+    name:string
+    established:boolean, 
+    is_costy:boolean, 
+}
+
+interface school_full_details extends school{
+head_master:string, 
+contact:number, 
+teachers_number:number, 
+}
+
+const our_school:school_full_details = {
+    name: "Bhawal", 
+    established: false, 
+    is_costy: false, 
+    head_master: "Bhawal", 
+    contact: 19, 
+    teachers_number:40
+}
+
+console.table(our_school);
