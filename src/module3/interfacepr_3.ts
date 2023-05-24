511d238d2f6daae736a14ef45e6b2cf8954500d0
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-24 14:43:53 +0600

    done

---
+++ src/module3/interfacepr_3.ts
@@ -0,0 +1,4 @@
+//!n! irex  => interface => reference type(farab) => extends =>
+//!n! furab => function => array => object
+
+// ? In TypeScript, the extends keyword is used in interface declarations to create a new interface that extends an existing interface. When one interface extends another, it inherits all the properties and methods of the base interface, and can define its own additional properties and methods.
