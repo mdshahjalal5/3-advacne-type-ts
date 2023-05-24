ea256117aaf1e238dcb9ec053a703768152472d4
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-24 17:06:27 +0600

    done

---
+++ src/module3/generic-function_pr2.ts
@@ -0,0 +1,12 @@
+const picnic_arrange = <T1, T2>(p1:T1, p2:T2)=>{
+
+    return {
+        ...p1, 
+        gurdian:`my Gurdian is${p2}`
+    }
+
+}
+
+const picnic = picnic_arrange<object, string>({}, 'Kamruzzaman');
+console.log(picnic);
+
