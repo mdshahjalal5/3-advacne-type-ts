1352f5e4eb3347966ce669c0e0d519fc53ee74e4
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-25 07:55:46 +0600

    done

--- src/module3/conditional-type_pr2.ts
+++ src/module3/conditional-type_pr2.ts
@@ -1,21 +1,12 @@
 // ?  kich => check key => check a specific key type  exist or not   exist in a object type 
 
-interface college{
-    name: string;
-    established:number, 
-    student:number, 
+interface College {
+  name: string;
+  established: number;
+  student: number;
 }
 
-
-
-
-interface college{
-    name: string;
-    established:number, 
-    student:number, 
-}
-
-const college_keys = keyof college;
+type CollegeKeys = keyof College;
 
 
 
