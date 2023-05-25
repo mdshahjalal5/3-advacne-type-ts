736c60ed80490729fe9d02ecc50e77836703c98f
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-25 07:58:38 +0600

    done

--- src/module3/conditional-type_pr2.ts
+++ src/module3/conditional-type_pr2.ts
@@ -1,13 +1,15 @@
 // ?  kich => check key => check a specific key type  exist or not   exist in a object type 
-
+// ? kig => keyof> generic> 
 interface College {
   name: string;
   established: number;
   student: number;
 }
 
+// ? keyof 
 type CollegeKeys = keyof College;
 
+// ? generic  
 type check_key<x,y> = y extends CollegeKeys ? true : false;
 
 type check_teacher = check_key<string,"teacher">
