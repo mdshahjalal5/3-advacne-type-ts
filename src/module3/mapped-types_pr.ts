e660c38d3133e65c189e38a0177c8842196d90b9
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-25 12:54:08 +0600

    done

--- src/module3/mapped-types_pr.ts
+++ src/module3/mapped-types_pr.ts
@@ -41,7 +41,7 @@ type is_android = phone_config["is_android"]
 
 // !n! in operator 
 type phone2 = {
-    [key in 'name'|'color'] :string // !n! take name & color  and generate type 
+    [key_anything in 'name'] :string // !n! take name & color  and generate type 
 }
 
 type Area2<T> = {
